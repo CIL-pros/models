0f5dec43593f8129ee9ff06e308c82edc0cffb25
andisheh94 <andisheh.amrollahi@gmail.com>
2018-06-30 01:54:41 +0200

    Fixed bugs in PCA. Still haven't used flags and crop and batch szize are hardcoded

--- research/deeplab/local_test.sh
+++ research/deeplab/local_test.sh
@@ -83,9 +83,9 @@ python "${WORK_DIR}"/train.py \
   --atrous_rates=18 \
   --output_stride=16 \
   --decoder_output_stride=4 \
-  --train_crop_size=608 \
-  --train_crop_size=608 \
-  --train_batch_size=32 \
+  --train_crop_size=400 \
+  --train_crop_size=400 \
+  --train_batch_size=10 \
   --training_number_of_steps="${NUM_ITERATIONS}" \
   --fine_tune_batch_norm=true \
   --tf_initial_checkpoint="${INIT_FOLDER}/deeplabv3_pascal_train_aug/model.ckpt" \
